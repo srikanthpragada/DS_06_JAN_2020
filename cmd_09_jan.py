f0423df5b202879f5de99c085801c0947c2e739f
srikanthpragada <srikanthpragada@gmail.com>
2020-01-09 08:57:21 +0530

    09-JAN

---
+++ cmd_09_jan.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+10 + 20
+from datetime import datetime
+datetime.now()
+for n in range(10,21):
+    print(n * n,end=' ')
+for n in range(10,21):
+    print(n * n,end=' ')
+def iseven(n):
+    return  n % 2 == 0
+get_ipython().run_line_magic('pwd', '')
+get_ipython().run_line_magic('system', 'dir')
+get_ipython().run_line_magic('system', 'type names.txt')
+get_ipython().run_line_magic('system', "type 'names.txt'")
